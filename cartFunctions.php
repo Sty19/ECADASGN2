ef6cf3ba2a82ddc369dc14f659f661de59e4e442
hopepx <hopeparadoxsolos@gmail.com>
2025-02-04 13:34:04 +0800

    fix typo

--- cartFunctions.php
+++ cartFunctions.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection
-$conn = new mysqli("localhost", "root", "", "your_database_name");
+$conn = new mysqli("localhost", "root", "", "ecadasgn1");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
