d269c45419e6f79c6f7ea7807b6133bc2fc45eed
hopepx <hopeparadoxsolos@gmail.com>
2025-02-06 00:24:42 +0800

    patch

--- header.php
+++ header.php
@@ -18,7 +18,7 @@ session_start();
 <body>
     <div class="welcome-message">
         <h1>Welcome To BabyJoy Store, <?php echo isset($_SESSION["ShopperName"]) ? $_SESSION["ShopperName"] : "Guest"; ?></h1>
-        <p>We are having sales from 1 Feb to 22 Feb!</p>
+        <p>We are having sales from 1 Feb to 21 Feb!</p>
     </div>
     <nav>
         <a href="index.php">Home</a>
@@ -32,21 +32,6 @@ session_start();
             <a href="login.php">Login</a>
         <?php endif; ?>
     </nav>
-     <!-- Home Section Start -->
-	<section class="home" id="home">
-		<div class="main-text">
-            <h5 data-text= "Summer Collection">Baby Essentials Collection</h5>
-            
-            <h1>Everything You Need</h1>
-			&nbsp;&nbsp;<h2>for Your Little One</h2>
-             
-            
-			<p>Explore our wide selection of adorable baby clothing, fun toys, and must-have essentials to keep your baby happy, comfortable, and stylish.</p>
-            <a href="products.php" class="main-btn">Explore Now <i class='bx bx-right-arrow-alt'></i></a>
-        </div>
-        </div>
-	</section>
-    <!-- Home Section End -->
 </body>
     
 </html>
