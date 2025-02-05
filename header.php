e671594429a118f35366899eb9b4e0e6cf85ff5f
hopepx <hopeparadoxsolos@gmail.com>
2025-02-05 23:58:15 +0800

    testing

--- header.php
+++ header.php
@@ -12,13 +12,13 @@ session_start();
     <title>BabyJoy e-BookStore</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="css/site.css">
+    <link rel="stylesheet" type="text/css" href="ECAD2024Oct_Assignment_1_Input_Files/css/styles.css">
 </head>
 
 <body>
     <div class="welcome-message">
         <h1>Welcome To BabyJoy Store, <?php echo isset($_SESSION["ShopperName"]) ? $_SESSION["ShopperName"] : "Guest"; ?></h1>
-        <p>We are having sales from 1 Feb to 21 Feb!</p>
+        <p>We are having sales from 1 Feb to 22 Feb!</p>
     </div>
     <nav>
         <a href="index.php">Home</a>
@@ -32,6 +32,21 @@ session_start();
             <a href="login.php">Login</a>
         <?php endif; ?>
     </nav>
+     <!-- Home Section Start -->
+	<section class="home" id="home">
+		<div class="main-text">
+            <h5 data-text= "Summer Collection">Baby Essentials Collection</h5>
+            
+            <h1>Everything You Need</h1>
+			&nbsp;&nbsp;<h2>for Your Little One</h2>
+             
+            
+			<p>Explore our wide selection of adorable baby clothing, fun toys, and must-have essentials to keep your baby happy, comfortable, and stylish.</p>
+            <a href="products.php" class="main-btn">Explore Now <i class='bx bx-right-arrow-alt'></i></a>
+        </div>
+        </div>
+	</section>
+    <!-- Home Section End -->
 </body>
-
+    
 </html>
