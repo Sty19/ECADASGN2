08ec73c7debc6a14b7b90b35725e972fe36f9e7b
hopepx <hopeparadoxsolos@gmail.com>
2025-02-08 18:32:02 +0800

    add profile frontend

--- profile.php
+++ profile.php
@@ -46,95 +46,98 @@ function validateForm()
     return true;
 }   
 </script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta ProductTitle="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to BabyJoy Store</title>
+    <link rel="stylesheet" type="text/css" href="ECAD2024Oct_Assignment_1_Input_Files/css/styles.css">
+    <link rel="stylesheet" type="text/css" href="ECAD2024Oct_Assignment_1_Input_Files/css/profile.css">
 
-<div style="width:80%; margin:auto;">
-<form name="register" action="editProfile.php" method="post" 
-      onsubmit="return validateForm()">
-    <div class="mb-3 row">
-        <div class="col-sm-9 offset-sm-3">
-            <span class="page-title">Membership Registration</span>
-        </div>
-    </div>
-    <div class="mb-3 row">
-        <label class="col-sm-3 col-form-label" for="name">Name:</label>
-        <div class="col-sm-9">
-            <input class="form-control" name="name" id="name" value="<?= htmlspecialchars($name) ?>" 
-                   type="text" required /> (required)
-        </div>
-    </div>
+    <!-- Box Icons -->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
 
-    <div class="mb-3 row">
-        <label class="col-sm-3 col-form-label" for="birthdate">Birth Date:</label>
-        <div class="col-sm-9">
-            <input class="form-control" name="birthdate" id="birthdate" 
-                   type="date" value="<?= htmlspecialchars($birthdate) ?>" />
-        </div>
-    </div>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
 
-    <div class="mb-3 row">
-        <label class="col-sm-3 col-form-label" for="address">Address:</label>
-        <div class="col-sm-9">
-            <textarea class="form-control" name="address" id="address"
-                      cols="25" rows="4"><?= htmlspecialchars($address) ?></textarea>
-        </div>
+</head>
+<body>
+<!-- Form Container -->
+<div class="container">
+        <form name="register" action="editProfile.php" method="post" onsubmit="return validateForm()">
+            <div class="mb-3 row">
+                <label class="col-form-label" for="name">Name:</label>
+                <div>
+                    <input class="form-control" name="name" id="name" value="<?= htmlspecialchars($name) ?>" type="text" required /> 
+                    <span class="required">(required)</span>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label class="col-form-label" for="birthdate">Birth Date:</label>
+                <div>
+                    <input class="form-control" name="birthdate" id="birthdate" type="date" value="<?= htmlspecialchars($birthdate) ?>" />
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label class="col-form-label" for="address">Address:</label>
+                <div>
+                    <textarea class="form-control" name="address" id="address" cols="25" rows="4"><?= htmlspecialchars($address) ?></textarea>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label class="col-form-label" for="country">Country:</label>
+                <div>
+                    <input class="form-control" name="country" id="country" type="text" value="<?= htmlspecialchars($country) ?>" />
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label class="col-form-label" for="phone">Phone:</label>
+                <div>
+                    <input class="form-control" name="phone" id="phone" type="text" value="<?= htmlspecialchars(substr($phone, 5)) ?>" />
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label class="col-form-label" for="email">Email Address:</label>
+                <div>
+                    <input class="form-control" name="email" id="email" type="email" required value="<?= htmlspecialchars($email) ?>" /> 
+                    <span class="required">(required)</span>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label class="col-form-label" for="password">Password:</label>
+                <div>
+                    <input class="form-control" name="password" id="password" type="password" required value="<?= htmlspecialchars($password) ?>" /> 
+                    <span class="required">(required)</span>
+                </div>
+            </div>
+            <p>If you forget your password, this question and answer will help allow you to retrieve your password.</p>
+            <div class="mb-3 row">
+                <label class="col-form-label" for="pwdquestion">Password Question:</label>
+                <div>
+                    <input class="form-control" name="pwdquestion" id="pwdquestion" type="text" required value="<?= htmlspecialchars($pwdquestion) ?>" /> 
+                    <span class="required">(required)</span>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label class="col-form-label" for="pwdanswer">Password Answer:</label>
+                <div>
+                    <input class="form-control" name="pwdanswer" id="pwdanswer" type="text" required value="<?= htmlspecialchars($pwdanswer) ?>" /> 
+                    <span class="required">(required)</span>
+                </div>
+            </div>
+            <input class="form-control" name="activestatus" id="activestatus" type="hidden" required value="<?= htmlspecialchars($activestatus) ?>" />
+            <input class="form-control" name="dateentered" id="dateentered" type="hidden" required value="<?= htmlspecialchars($dateentered) ?>" />
+            <input class="form-control" name="currentemail" id="currentemail" type="hidden" required value="<?= htmlspecialchars($currentemail) ?>" />
+            <div class="mb-3 row">
+                <div>
+                    <button type="submit">Save Edit</button>
+                </div>
+            </div>
+        </form>
     </div>
-    <div class="mb-3 row">
-        <label class="col-sm-3 col-form-label" for="country">Country:</label>
-        <div class="col-sm-9">
-            <input class="form-control" name="country" id="country" type="text" value="<?= htmlspecialchars($country) ?>" />
-        </div>
-    </div>
-    <div class="mb-3 row">
-        <label class="col-sm-3 col-form-label" for="phone">Phone:</label>
-        <div class="col-sm-9">
-            <input class="form-control" name="phone" id="phone" type="text" value="<?= htmlspecialchars(substr($phone, 5)) ?>" />
-        </div>
-    </div>
-    <div class="mb-3 row">
-        <label class="col-sm-3 col-form-label" for="email">
-            Email Address:</label>
-        <div class="col-sm-9">
-            <input class="form-control" name="email" id="email" 
-                   type="email" required value="<?= htmlspecialchars($email) ?>" /> (required)
-        </div>
-    </div>
-    <div class="mb-3 row">
-        <label class="col-sm-3 col-form-label" for="password">
-            Password:</label>
-        <div class="col-sm-9">
-            <input class="form-control" name="password" id="password" 
-                   type="password" required value="<?= htmlspecialchars($password) ?>" /> (required)
-        </div>
-    </div>
-
-    <p>If you forget your password, this question and answer will help allow you to retrieve your password</p>
-    <div class="mb-3 row">
-        <label class="col-sm-3 col-form-label" for="pwdquestion">
-            Password Question: </label>
-        <div class="col-sm-9">
-            <input class="form-control" name="pwdquestion" id="pwdquestion" 
-                   type="text" required value="<?= htmlspecialchars($pwdquestion) ?>" /> (required)
-        </div>
-    </div>
-
-    <div class="mb-3 row">
-        <label class="col-sm-3 col-form-label" for="pwdanswer">
-            Password Answer: </label>
-        <div class="col-sm-9">
-            <input class="form-control" name="pwdanswer" id="pwdanswer" 
-                   type="text" required value="<?= htmlspecialchars($pwdanswer) ?>" /> (required)
-        </div>
-    </div>
-    <input class="form-control" name="activestatus" id="activestatus" type="hidden" required value="<?= htmlspecialchars($activestatus) ?>" />
-    <input class="form-control" name="dateentered" id="dateentered" type="hidden" required value="<?= htmlspecialchars($dateentered) ?>" />
-    <input class="form-control" name="currentemail" id="currentemail" type="hidden" required value="<?= htmlspecialchars($currentemail) ?>" />
-    <div class="mb-3 row">       
-        <div class="col-sm-9 offset-sm-3">
-            <button type="submit">Save Edit</button>
-        </div>
-    </div>
-</form>
-</div>
 <?php 
 include("footer.php"); 
 ?>
+</body>
+</html>
